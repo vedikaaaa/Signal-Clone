c161a424b55befe05f50f6da7379336373765b50
vedikagupta <vedikagupta@microsoft.com>
2022-09-11 20:15:19 +0530

    new

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+     <ScrollView>
+      
+     </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
